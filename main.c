6a43906cdf02f3da5a50d156598c4e5d581090c1
CarlosNatyel <138235240+CarlosNatyel@users.noreply.github.com>
2023-08-23 19:35:30 -0300

    Update main.c

---
+++ main.c
@@ -0,0 +1,33 @@
+//Teste de Main.c
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include "funcionario.h"  // Inclui o arquivo de cabeçalho que contém as declarações das funções e a definição da estrutura Funcionario.
+
+//int main() {
+   // int tamanho;
+    //Funcionario *funcionarios = ler_funcionarios("funcionarios.txt", &tamanho);  // Lê os funcionários do arquivo e obtém o tamanho da lista.
+
+   // while (1) {
+        //char opcao[100];
+        //printf("Digite o nome ou documento do funcionario que deseja buscar (ou 'sair' para encerrar): ");
+        //scanf("%s", opcao);  // Lê a opção (nome ou documento do funcionário ou 'sair') inserida pelo usuário.
+
+       // if (strcmp(opcao, "sair") == 0) {
+         //   break;  // Se o usuário digitar 'sair', o loop é encerrado e o programa termina.
+       // }
+
+       // Funcionario *resultado = busca_binaria_por_nome(funcionarios, tamanho, opcao);  // Realiza a busca binária pelo nome ou documento inserido.
+
+       // if (resultado) {
+        //    printf("Funcionario encontrado - Nome: %s, Cargo: %s, Documento: %s\n",
+         //          resultado->nome, resultado->cargo, resultado->documento);  // Exibe as informações do funcionário encontrado.
+       // } else {
+       //     printf("Funcionario nao encontrado.\n");  // Exibe mensagem caso o funcionário não seja encontrado.
+       // }
+  //  }
+
+   // liberar_memoria(funcionarios);  // Libera a memória alocada para a lista de funcionários.
+    //return 0;  // Retorna 0 para indicar que o programa foi executado com sucesso.
+}
+
