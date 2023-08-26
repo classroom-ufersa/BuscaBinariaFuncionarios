7f409521928c58292ef14c4709dcd77230798a86
CarlosNatyel <138235240+CarlosNatyel@users.noreply.github.com>
2023-08-26 18:48:14 -0300

    Update funcionario.h

--- funcionario.h
+++ funcionario.h
@@ -1,1 +1,13 @@
 //Implementação da interface das funções
+
+//typedef struct Funcionario {
+//    char nome[100];
+//    char cargo[100];
+//    char documento[20];
+//};
+
+//Funcionario *ler_funcionarios(char *nome_arquivo, int *tamanho);
+//Funcionario *busca_binaria_por_nome(Funcionario *funcionarios, int tamanho, char *nome_alvo);
+//void liberar_memoria(Funcionario *funcionarios);
+
+
