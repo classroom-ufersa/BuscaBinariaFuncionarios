d77936f5408190086873e35f5c539c853606d3c2
CarlosNatyel <138235240+CarlosNatyel@users.noreply.github.com>
2023-09-02 16:23:13 -0300

    Update main.c

--- main.c
+++ main.c
@@ -18,26 +18,58 @@ int main(void)
     switch(opcao1)
     {
       case 1:
-      resgitra_funcionario(funcionario, arquivo); break;
+      resgitra_funcionario(funcionario, arquivo); 
+      break;
       
+  
       case 2:
-      nfuncionarios = quantifica_funcionarios(arquivo);
+            nfuncionarios = quantifica_funcionarios(arquivo);
+            funcionario = carrega_dados(funcionario, arquivo, nfuncionarios);
 
-      funcionario = carrega_dados(funcionario, arquivo, nfuncionarios); // Aloque memória aqui
+            printf("1 - Buscar por Nome\n");
+            printf("2 - Buscar por Documento\n");
+            scanf(" %d", &opcao2);
+            switch (opcao2)
+            {
+            case 1:
+                buscaBinariaNome(funcionario, nfuncionarios);
+                int posicaoNome = buscaBinariaNome(funcionario, nfuncionarios);
+                if (posicaoNome == -1)
+                {
+                    printf("Nome não encontrado\n");
+                }
+                else
+                {
+                    printf("Nome encontrado na posição %d\n", posicaoNome);
+                }
+                break;
 
-      buscaBinariaNome(funcionario, nfuncionarios);
-      int posicao = buscaBinariaNome(funcionario, nfuncionarios);
-    if (posicao == -1) {
+            case 2:
+                buscaBinariaDocumento(funcionario, nfuncionarios);
+                int posicaoDocumento = buscaBinariaDocumento(funcionario, nfuncionarios);
+                if (posicaoDocumento == -1)
+                {
+                    printf("Documento não encontrado\n");
+                }
+                else
+                {
+                    printf("Documento encontrado na posição %d\n", posicaoDocumento);
+                }
+                break;
+
+    if (posicaoNome == -1) {
         printf("Valor não encontrado\n");
     } else {
-        printf("Valor encontrado na posição %d\n", posicao);
+        printf("Valor encontrado na posição %d\n", posicaoNome);
     }
       
       libera_funcionarios(funcionario);
       break; 
     }
+
   } while(opcao1 != 3);
 
   return 0;
 }
 
+}
